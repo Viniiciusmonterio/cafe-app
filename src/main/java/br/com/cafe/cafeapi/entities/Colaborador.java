4d1572d7325f4fce2ab42d3b0e9b2e442450439b
Vinícius Monteiro <47702719+Viniiciusmonterio@users.noreply.github.com>
2022-04-09 01:38:19 -0300

    subindo aplicacao

--- src/main/java/br/com/cafe/cafeapi/entities/Colaborador.java
+++ src/main/java/br/com/cafe/cafeapi/entities/Colaborador.java
@@ -4,15 +4,17 @@ import java.io.Serializable;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 public class Colaborador implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(generator = "increment")
+	@GenericGenerator(name = "increment", strategy = "increment")
 	private Long id;
 	private String nome;
 	private String cpf;
